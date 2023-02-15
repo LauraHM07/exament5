d1ed3051580581689be0f2d244b50a24cf9f84f1
Laura Haro <lauraharomolina@gmail.com>
2023-02-15 12:06:36 +0100

    controller

--- src/main/java/com/oretania/portal/controllers/AsignaturaController.java
+++ src/main/java/com/oretania/portal/controllers/AsignaturaController.java
@@ -9,32 +9,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AsignaturaController {
     
     @RequestMapping(value = "/matematicas")
-    @PreAuthorize("hasAnyAuthority('Matemáticas')")
+    @PreAuthorize("hasAuthority('Matemáticas')")
     public String matematicas() {
         return "matematicas";
     }
 
     @RequestMapping(value = "/lengua")
+    @PreAuthorize("hasAuthority('Lengua)")
     public String lengua() {
         return "lengua";
     }
 
     @RequestMapping(value = "/ingles")
+    @PreAuthorize("hasAuthority('Inglés')")
     public String ingles() {
         return "ingles";
     }
 
     @RequestMapping(value = "/religion")
+    @PreAuthorize("hasAuthority('Religión')")
     public String religion() {
         return "religion";
     }
 
     @RequestMapping(value = "/fisica")
+    @PreAuthorize("hasAuthority('Física')")
     public String fisica() {
         return "fisica";
     }
 
     @RequestMapping(value = "/quimica")
+    @PreAuthorize("hasAuthority('Química')")
     public String quimica() {
         return "quimica";
     }
